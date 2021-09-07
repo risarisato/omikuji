fc9e1d01ad1b1a5831b5cf681728eb76c8bda3a1
りさりさと <88628553+risarisato@users.noreply.github.com>
2021-09-07 21:10:04 +0900

    おみくじゲームを追加

---
+++ omikuji.js
@@ -0,0 +1,33 @@
+var omikujiResult = [
+    "大吉",
+    "中吉",
+    "小吉",
+    "凶" 
+  ];
+
+  $(function() {
+    $("#omikujiBtn").click(function() {
+      let btn = $(this);
+      btn.prop("disabled", true);
+      // おみくじを振る
+      $("#omikujiImg").addClass("buruburu");
+      // ブルブルを止める
+      setTimeout(function() {
+        $("#omikujiImg").removeClass("buruburu");
+      }, 1500);
+      // おみくじをひっくり返す
+      setTimeout(function() {
+        $("#omikujiImg").addClass("rotate");
+      }, 3000);
+      // 結果が表示される
+      setTimeout(function() {
+        var result = omikujiResult[Math.floor( Math.random() * omikujiResult.length)];
+        $("#omikujiResult").html("<div class='omikujiResultInner'>" + result + "</div>");
+      }, 5000);
+      // リセットする
+      setTimeout(function() {
+        $("#omikujiResult").html("<img id='omikujiImg' src='omikuji.png'>");
+        btn.prop("disabled", false);
+      }, 8000);
+    });
+  });
